90a1b1d2eeb6e53ff1a5f8088485de90f45d0f5f
ManuelPrhyme <manuelbagoole@gmail.com>
2025-08-11 09:20:54 +0300

    Con

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,21 +26,21 @@ export default function Home() {
   //     const accounts =  await WalletClient.requestPermissions({eth_accounts:{}})
   // } 
 
-   const assync = async() => {
-    await sdk.actions.ready();}
+  //  const assync = async() => {
+  //   await sdk.actions.ready();}
 
-    assync()
+  //   assync()
 
-//   useEffect(()=>{
-//     const SDK =  async () => {
-//     await sdk.actions.ready();
-//     setIsReady(true)
-//   }
+  useEffect(()=>{
+    const SDK =  async () => {
+    await sdk.actions.ready();
+    setIsReady(true)
+  }
 
-//   if(!isReady){
-//     SDK();}
+  if(!isReady){
+    SDK();}
   
-// },[isReady])
+},[isReady])
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
