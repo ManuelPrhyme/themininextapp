1509d0a9088344db3b4ff74bd5c407b7ab42e28f
ManuelPrhyme <manuelbagoole@gmail.com>
2025-08-11 08:27:16 +0300

    Connect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,6 +36,10 @@ export default function Home() {
 
   const [isReady,setIsReady] = useState(false)
 
+  const connect = async () => {
+      const accounts =  await window.ethereum.request({method:"eth_requestAccounts"})
+  } 
+
    const assync = async() => {
     await sdk.actions.ready();}
 
@@ -78,8 +82,8 @@ export default function Home() {
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="rounded-full cursor-pointer border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            onClick = {connect} 
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -90,7 +94,7 @@ export default function Home() {
               width={20}
               height={20}
             />
-            Deploy now
+            Connect
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
